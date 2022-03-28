1f6f8ff95c9f55757e0310430633e9f8533f729c
AE210217 <eunjanii@etoos.com>
2022-03-28 14:29:59 +0900

    Dollar 클래스 선언

---
+++ MoneyTDD/app/src/main/java/com/happy/moneytdd/Dollar.java
@@ -0,0 +1,13 @@
+package com.happy.moneytdd;
+
+public class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        
+    }
+}
