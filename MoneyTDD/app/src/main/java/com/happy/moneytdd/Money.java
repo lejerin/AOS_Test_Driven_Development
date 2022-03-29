1b7039f9ec29a88bd1709ef72173aa481cc5be90
AE210217 <eunjanii@etoos.com>
2022-03-29 10:38:03 +0900

    컴파일 에러 해결 : 1. Money abstrac 메소드 currency() 선언 2. Dollar, Franc 메소드 구현

--- MoneyTDD/app/src/main/java/com/happy/moneytdd/Money.java
+++ MoneyTDD/app/src/main/java/com/happy/moneytdd/Money.java
@@ -4,6 +4,7 @@ public abstract class Money {
     protected int amount;
 
     abstract Money times(int multiplier);
+    abstract String currency();
 
     static Money dollar(int amount) {
         return new Dollar(amount);
