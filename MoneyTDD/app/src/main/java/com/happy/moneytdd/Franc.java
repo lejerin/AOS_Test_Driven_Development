f9e5af020db68b373c03ee7ff2353be7cbcaa3e9
lejerin <lejerin1112@gmail.com>
2022-04-04 09:50:26 +0900

    중복 Dollar, Franc 제거 & 테스트 코드 정리

--- MoneyTDD/app/src/main/java/com/happy/moneytdd/Franc.java
+++
@@ -1,8 +0,0 @@
-package com.happy.moneytdd;
-
-public class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- MoneyTDD/app/src/test/java/com/happy/moneytdd/MoneyTest.java
+++ MoneyTDD/app/src/test/java/com/happy/moneytdd/MoneyTest.java
@@ -23,8 +23,6 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -32,10 +30,5 @@ public class MoneyTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "USD").equals(new Dollar(10, "USD")));
     }
 }
