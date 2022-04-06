dac33e6e3206f4c59144332740609f7016f74fa4
lejerin <lejerin1112@gmail.com>
2022-04-06 13:44:43 +0900

    testReduceMoneyDifferent() 테스트 코드 작성

--- MoneyTDD/app/src/main/java/com/happy/moneytdd/Bank.java
+++ MoneyTDD/app/src/main/java/com/happy/moneytdd/Bank.java
@@ -1,14 +1,23 @@
 package com.happy.moneytdd;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
 
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD"))
-                ? 2
-                : 1;
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
+
 }
